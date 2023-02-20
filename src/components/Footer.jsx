03a86238208af56b0c2b40f689cbbda397ad91d0
triadi <triadilaksamana@gmail.com>
2023-02-20 20:21:48 +0700

    update Hero link and placeholder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,24 +6,27 @@ import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
-  return (
-    <footer className="footer">
-      <Container>
-        <Row className="align-items-center">
-          <MailchimpForm />
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
-            </div>
-            <p>Copyright 2022. All Rights Reserved</p>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
-  )
-}
+    return (
+        <footer className="footer">
+            <Container className="">
+                <Row className="d-flex align-items-center justify-content-center">
+                    <MailchimpForm />
+                    <Col className="text-center text-sm-around">
+                        <div className="social-icon">
+                            <a href="https://www.linkedin.com/in/tri-adi-2249621ab/">
+                                <img src={navIcon1} alt="Linkedin" />
+                            </a>
+                            <a href="https://www.instagram.com/bangyadiii/">
+                                <img src={navIcon3} alt="" />
+                            </a>
+                            <a href="https://github.com/bangyadiii">
+                                <img src={navIcon3} alt="" />
+                            </a>
+                        </div>
+                        <p>Copyright 2023. All Rights Reserved</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+};
